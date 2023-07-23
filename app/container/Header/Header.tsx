435ab0c683c9f6a0b7b4f588c1d6270d88254690
Anurag Kumar <anuragabcr@gmail.com>
2023-07-24 02:59:52 +0530

    header completed

--- app/container/Header/Header.tsx
+++ app/container/Header/Header.tsx
@@ -1,8 +1,77 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+import { images } from "@/app/constants";
 import "./Header.scss";
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+
 const Header = () => {
-  return <div>Header </div>;
+  return (
+    <div id="home" className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Anurag</h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Full Stack Developer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <Image src={images.profile} alt="profile_bg" />
+        <motion.div
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+        >
+          <Image
+            src={images.circle}
+            alt="profile_circle"
+            className="overlay_circle"
+          />
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <Image src={circle} alt="profile_bg" />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
 };
 
 export default Header;
